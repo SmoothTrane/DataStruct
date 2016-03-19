e53a5d350aefa9b9292fd094286935fc1255f29a
Gary <gary.coltrane1@marist.edu>
2016-03-19 02:28:08 -0400

    New Changes

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,39 +1,79 @@
 
 public class ArrayList implements List{
+private int[] arr;
+private int size;
+public ArrayList(int val){
+	arr = new int[val];
+	size = 0;
+	//USER MUST SPECIFY a CAPACITY on the array in order to add elements 
+}
 
 	
 	public void add(int n){
-	int[] arr = {1,2,3,4};
-int[] newArray = new int [arr.length];
-for(int i =0; i<arr.length; i++){
-	
-	newArray[i] = arr[i];
-}
-newArray[newArray.length -1] = n;
+	arr[size] = n;
+	size++;
+	// O(1) 
 
 	}
 	
 	public boolean update(int currentN, int replaceN){
-		int[] arr = {1,2,3,4};
-		for(int i=0; i<arr.length; i++){
-		if(arr[i] == currentN){
-			arr[i] = replaceN;
-			return true;
-		}
 	
-		}
-return false;
-	}
-	
-	public int[] delete (int[] arr, int n ){
-	
-		int[] newArr  = {arr.length-1};
-	for(int i =0; i<arr.length; i++){
-		if(arr[i] != n){
-			newArr[i] = arr[i];
-		}
-	}
-	return newArr;
-	}
+int i=0;
+while(arr[i]!=currentN){
+	i++;
+	if(i>=size){
+		return false;
 	}
 
+}
+
+arr[i]= replaceN;
+
+return true;
+
+}
+		
+
+	
+	public String toString(){
+		if(size==0){
+			return "Array is empty: []";
+		}
+		String ret = "[" + arr[0];
+		for(int i=1; i<size; i++){
+			ret += "," + arr[i];
+		}
+		ret += "]";
+		return ret;
+	}
+	public boolean contains(int n){
+		
+		for(int i=0; i<arr.length; i++){
+			if(arr[i] == n){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+	
+	public boolean delete(int n){
+		int[] arr = {1,2,3,4};
+	int[] newArr = new int[arr.length-1];
+		for(int i = 0; i<arr.length; i++){
+			if(arr[i] != n){
+				newArr[i] = arr[i];
+			}
+			
+		}
+		
+	return true;
+
+		
+	}
+	
+	public int size(){
+		return size;
+	}
+}
--- src/Driver.java
+++ src/Driver.java
@@ -3,9 +3,14 @@ public class Driver {
 public static void main(String[] args){
 	
 	
-List array = new ArrayList();
-int[] arr = {8,9,10,3,4};
-System.out.println(array.delete(arr, 10));
+List array = new ArrayList(5);
+array.add(1);
+array.update(1, 3);
+array.add(5);
+array.add(30);
+array.update(30, 20);
+System.out.println(array.toString());
+
 	}
 
 
